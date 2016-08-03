bf75d89f10add054961201f38823dbf46d2c37a9
dillon <dillon@DishPC.fios-router.home>
2016-08-03 15:23:47 -0400

    swapped dual-panel UI for a single panel layout

--- arcjava/src/arcjava/SelectionPanel.java
+++ arcjava/src/arcjava/SelectionPanel.java
@@ -5,138 +5,63 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class SelectionPanel extends JPanel {
-
-	// This is the JPanel which handles the initial screen of the program, where the user selects whether to create a new item or select
-	// an existing item to use as a base.  This panel is composed of two subpanels - leftPanel and rightPanel.  leftPanel is
-	// always visible and controls which panel is shown on rightPanel via a pair of radio buttons which activate their corresponding panel.	
 	
-	public SelectionPanel() {
-		
-		JPanel leftPanel = new JPanel();
-		
-		leftPanel.setSize(300, 400);
-		leftPanel.setLayout(new GridLayout(3, 1));
-		leftPanel.setBorder(BorderFactory.createLineBorder(Color.gray));
-		
-		ButtonGroup neGroup = new ButtonGroup();
-		JRadioButton newRad = new JRadioButton("New");
-		neGroup.add(newRad);
-		JRadioButton existingRad = new JRadioButton("Existing");
-		neGroup.add(existingRad);
-		JLabel neTitle = new JLabel("Create a New Item or Select an Existing Item");
-		
-		leftPanel.add(neTitle);
-		leftPanel.add(newRad);
-		leftPanel.add(existingRad);
-		
-		leftPanel.setVisible(true);	
-		
-		NewPanel newPanel = new NewPanel(); // COGNITIVE DISSONANCE, HO!
-		newPanel.setVisible(false);
-		
-		ExistingPanel existingPanel = new ExistingPanel();
-		existingPanel.setVisible(false);
-		
-		ActionListener newListener = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				newPanel.setVisible(true);
-				existingPanel.setVisible(false);
-			}		
-		};
-		
-		ActionListener existingListener = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				newPanel.setVisible(false);
-				existingPanel.setVisible(true);
-			}		
-		};
-		
-		newRad.addActionListener(newListener);
-		existingRad.addActionListener(existingListener);
-		
-		this.add(leftPanel, BorderLayout.LINE_START);
-		this.add(newPanel);
-		this.add(existingPanel);
-		
-		this.setVisible(true);
-	}
-	
-}
-
-final class NewPanel extends JPanel {
-	
-	NewPanel() {
+	SelectionPanel() {
 		
 		this.setSize(500, 400);
-		this.setLayout(new GridLayout(5, 1));
+		this.setLayout(new GridLayout(7, 1));
 		
-		JLabel userLabel = new JLabel("Name");
-		JTextField userLine = new JTextField("Name, max 10 characters", 20);
-		
-		JLabel descLabel = new JLabel("Description");
-		JTextField descLine = new JTextField("Description, max 50 characters",20);
-		
-		JButton newButton = new JButton("Create Item");
-		
-		// TODO: add functionality for the button
-		ActionListener nButtonListener = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}		
-		};
-		
-		newButton.addActionListener(nButtonListener);
-		
-		this.add(userLabel);
-		this.add(userLine);
-		this.add(descLabel);
-		this.add(descLine);
-		this.add(newButton);
-		
-	}
-	
-}
-
-final class ExistingPanel extends JPanel {
-	
-	ExistingPanel() {
-		
-		this.setSize(500, 400);
-		this.setLayout(new GridLayout(5, 1));
-		
-		JLabel existingLabel = new JLabel("Select Item");
+		JLabel selectionLabel = new JLabel("Select Item");
 		
 		// TODO: swap out Strings for dual-String type (may have to hard code it)
-		String[] itemNames = {"F0006", "F0016WORLD", "F0101", "F01012", "F0311A"};
-		JComboBox existingList = new JComboBox(itemNames);
-		existingList.setBackground(Color.white);
+		String[] existingNames = {"F0006", "F0016WORLD", "F0101", "F01012", "F0311A"};
+		JComboBox selectionList = new JComboBox();
+		selectionList.setBackground(Color.white);
+		selectionList.addItem("");
+		selectionList.addItem("Create New Item");
+		for (int i = 0; i < existingNames.length; i++) {
+			selectionList.addItem(existingNames[i]);
+		}
 		
-		// TODO: add ComboBox functionality (is this even necessary?)
-		ActionListener eComboListener = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}		
-		};
+		JButton selectionButton = new JButton("Select Item");
 		
-		existingList.addActionListener(eComboListener);
-		
-		JButton existingButton = new JButton("Select Item");
+		JLabel nameLabel = new JLabel("Name:");
+		JTextField selectedName = new JTextField("");
+		selectedName.setBackground(Color.white);
+		selectedName.setEditable(false);
+		JLabel descLabel = new JLabel("Description:");
+		JTextField selectedDesc = new JTextField("");
+		selectedDesc.setBackground(Color.white);
+		selectedDesc.setEditable(false);
+
 		
 		// TODO: add button functionality
-		ActionListener eButtonListener = new ActionListener() {
+		ActionListener sButtonListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			}		
 		};
 		
-		JLabel selectedName = new JLabel("");
-		JLabel selectedDesc = new JLabel("");
 		
-		this.add(existingLabel);
-		this.add(existingList);
-		this.add(existingButton);
+		// TODO: add ComboBox functionality (is this even necessary?)
+		ActionListener sComboListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}		
+		};
+		
+		selectionList.addActionListener(sComboListener);
+
+		
+		
+		this.add(selectionLabel);
+		this.add(selectionList);
+		this.add(selectionButton);
+		this.add(nameLabel);
 		this.add(selectedName);
+		this.add(descLabel);
 		this.add(selectedDesc);
 				
-	}	
+	}
+	
 }
