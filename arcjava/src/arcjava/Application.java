62edcb3e55db562b0e081f106356a14ccd25e960
dillon <dillon@DishPC.fios-router.home>
2016-08-03 15:37:53 -0400

    swapped dual-panel UI for a single panel layout

--- arcjava/src/arcjava/Application.java
+++ arcjava/src/arcjava/Application.java
@@ -10,7 +10,7 @@ public class Application {
 	
 	private static void ProgramStart() {
 		JFrame frame1 = new JFrame("AS400 Interfacing");
-		frame1.setSize(800, 400);
+		frame1.setSize(500, 400);
 		frame1.setResizable(false);
 		frame1.add(new SelectionPanel(), BorderLayout.LINE_START);
 		frame1.setVisible(true);
